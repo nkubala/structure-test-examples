6b963f39226735c9bd75073d391b64fb2360b662
Nick Kubala <nkubala@google.com>
2017-11-28 11:10:43 -0800

    fix travis yaml

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+set -ex
+
 echo "Retrieving structure-test binary...."
 echo
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	curl -LO https://storage.googleapis.com/gcp-container-tools/structure-test/latest/structure-test-darwin-amd64 \
 	&& mv structure-test-darwin-amd64 structure-test \
 	&& sudo cp structure-test /usr/local/bin \
-	&& chmod +x /usr/local/bin/structure-test
+	&& sudo chmod +x /usr/local/bin/structure-test
 else
 	curl -LO https://storage.googleapis.com/gcp-container-tools/structure-test/latest/structure-test-linux-amd64 \
 	&& mv structure-test-linux-amd64 structure-test \
 	&& sudo cp structure-test /usr/local/bin \
-	&& chmod +x /usr/local/bin/structure-test
+	&& sudo chmod +x /usr/local/bin/structure-test
 fi
 
-mkdir -p $HOME/bin \
-&& export PATH=$PATH:$HOME/bin \
-&& mv structure-test $HOME/bin/structure-test
+export PATH=$PATH:/usr/local/bin
 
 echo
 echo "Pulling Google Python Docker image...."
